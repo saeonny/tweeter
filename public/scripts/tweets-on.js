61cad51740e05b616c96b5fd4fb802b3f13936ba
Sae <winny9641@gmail.com>
2022-06-23 02:03:45 -0400

    shadow added

--- public/scripts/tweets-on.js
+++ public/scripts/tweets-on.js
@@ -2,16 +2,18 @@ $(document).ready(function () {
 
 
   const $prevTweets = $('.prev-tweets')
-  const $flag = $('.fa-solid fa-flag')
-  const $re = $('.fa-solid fa-retweet')
-  const $heart = $('.fa-solid fa-heart')
+  const $flag = $('#flag')
+  const $re = $('#retweet')
+  const $heart = $('#heart')
+
+  const img = $('#tweets-img')
  
 
 
 
   $prevTweets.mouseover(() => {
     $prevTweets.css({
-      'box-shadow': '5px 5px #545149'
+      'box-shadow': '5px 5px #4056A1'
     })
   }
   )
@@ -27,11 +29,37 @@ $(document).ready(function () {
     $flag.css({
       'color' : '#4056A1'
     })
-    console.log("on")
+    
   })
 
   $flag.mouseleave(() => {
     $flag.css({
+      'color' : '#545149'
+    })
+  })
+
+  $re.mouseover(() => {
+    $re.css({
+      'color' : '#4056A1'
+    })
+    
+  })
+
+  $re.mouseleave(() => {
+    $re.css({
+      'color' : '#545149'
+    })
+  })
+
+  $heart.mouseover(() => {
+    $heart.css({
+      'color' : '#4056A1'
+    })
+    
+  })
+
+  $heart.mouseleave(() => {
+    $heart.css({
       'color' : '#545149'
     })
   })
